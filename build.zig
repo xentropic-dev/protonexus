bc49db2cf8e0c5a65aa8724cefa68f6ad065d599
Chris Laponsie <claponsie@kvcc.edu>
2025-08-06 17:00:13 -0400

    Added target parsing and validation

--- build.zig
+++ build.zig
@@ -1,7 +1,42 @@
 const std = @import("std");
+const builtin = @import("builtin");
+const Query = std.Target.Query;
 
-pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
+fn resolve_target(b: *std.Build, target_requested: ?[]const u8) !std.Build.ResolvedTarget {
+    const target_host = @tagName(builtin.target.cpu.arch) ++ "-" ++ @tagName(builtin.target.os.tag);
+    const target = target_requested orelse target_host;
+    const triples = .{
+        "x86_64-linux",
+        "x86_64-macos",
+        "x86_64-windows",
+    };
+    const cpus = .{
+        "x86_64_v3+aes",
+        "x86_64_v3+aes",
+        "x86_64_v3+aes",
+    };
+
+    const arch_os, const cpu = inline for (triples, cpus) |triple, cpu| {
+        if (std.mem.eql(u8, target, triple)) break .{ triple, cpu };
+    } else {
+        std.log.err("unsupported target: '{s}'", .{target});
+        return error.UnsupportedTarget;
+    };
+    const query = try Query.parse(.{
+        .arch_os_abi = arch_os,
+        .cpu_features = cpu,
+    });
+    return b.resolveTargetQuery(query);
+}
+
+
+pub fn build(b: *std.Build) !void {
+    const build_options = .{
+        .target = b.option([]const u8, "target", "The CPU architecture and OS to build for"),
+};
+    const target = try resolve_target(b, build_options.target);
+    const target_triple = try target.query.zigTriple(b.allocator);
+    std.debug.print("Building for target: {s}\n", .{target_triple});
 
     const optimize = b.standardOptimizeOption(.{});
 
@@ -45,9 +80,12 @@ pub fn build(b: *std.Build) void {
 
     // Required for TLS in open62541
     // TODO: Figure out how to link these for windows.
-    exe.linkSystemLibrary("mbedtls");
-    exe.linkSystemLibrary("mbedx509");
-    exe.linkSystemLibrary("mbedcrypto");
+    const lib_path = try std.mem.concat(b.allocator, u8, &.{ "lib/", target_triple, "/" });
+    
+    // exe.linkLibrary("mbedtls");
+    // exe.linkLibrary("mbedx509");
+    // exe.linkLibrary("mbedcrypto");
+    //
 
     b.installArtifact(exe);
 
